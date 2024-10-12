2c54a6c3dee2ecba877c862b39e958a876180091
DESKTOP-82J80DL\bkr_man <luinungom@gmail.com>
2024-10-13 00:12:30 +0200

    no message

---
+++ pythonEco/eco.py
@@ -0,0 +1,3 @@
+print('Juego del "eco, eco", dime algo y yo lo repito')
+frase: str = input('¿eco? ')
+print('eco!', frase)
---
+++ pythonEco/name.py
@@ -0,0 +1,4 @@
+name: str = input('Please, tell me your name: ')
+firstSurname: str = input('Please, tell me your first surname: ')
+secondSurname: str = input ('Please, tell me your second surname: ')
+print('Your complete info is:', name.upper(), firstSurname.upper(), secondSurname.upper())
